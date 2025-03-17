9882b3f359e3b0524bf218c3f240f04926d1ddb1
Mathieu M. <86263256+mathieumzl@users.noreply.github.com>
2025-03-17 16:21:24 +0100

    update v1 17/03/25

--- vue_typescript_vuetify_i18n/src/router/index.ts
+++ vue_typescript_vuetify_i18n/src/router/index.ts
@@ -45,6 +45,9 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: contact
+  },
+  { path: '*', 
+    redirect: '/' 
   }
 ]
 
